cae2e663bf0db5c0b63877898932d64984c4f1bf
Abbas Rouhi Hansen <rouhi438@gmail.com>
2025-09-02 12:03:49 +0200

    Adding the entire CVTemplate project to GitHub at once

---
+++ CVTemplate/scrip.js
@@ -0,0 +1,27 @@
+//======= I get image upload file  from AI ======///
+
+const input = document.getElementById("photoInput");
+const preview = document.getElementById("preview");
+const label = document.getElementById("uploadLabel");
+
+input.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      preview.src = e.target.result;
+      preview.style.display = "block"; // عکس نشون داده میشه
+      label.style.display = "none"; // SVG مخفی میشه
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+//========================================//
+
+const degreeLanguage = document.querySelectorAll(".deg");
+degreeLanguage.forEach((element) => {
+  element.addEventListener("click", function () {
+    element.classList.toggle("degBG");
+  });
+});
--- ToDoList/script.js
+++ ToDoList/script.js
@@ -5,6 +5,16 @@ const listToDo = document.querySelector("ul");
 const searchIcon = document.querySelector(".search-icon");
 const searchInput = document.querySelector(".search");
 const radioBtn = document.querySelector(".radio");
+
+//defining empty array for adding tasks in the local storage to prevent of removing
+let items;
+if (!localStorage.getItem("newTask")) {
+  items = [];
+} else {
+  items = getTask();
+}
+console.log(items);
+
 /// === adding new NODE to DOM ===============================
 addBtn.addEventListener("click", function () {
   let el = input.value;
@@ -14,6 +24,7 @@ addBtn.addEventListener("click", function () {
     alert("please fill the field.");
     return;
   }
+  saveTasks(el);
   newLi.innerHTML += `<input class="checkbox" type="radio" />`;
   newLi.innerHTML += `<span class="trash"><i class="fa-solid fa-trash-can"></i></span>`;
   listToDo.appendChild(newLi);
@@ -26,6 +37,28 @@ function createNew(el) {
   li.textContent = el;
   return li;
 }
+
+//========Adding tasks to localStorage for prevent of removing after the loading page or turn off
+function saveTasks(task) {
+  items.push(task);
+  localStorage.setItem("newTask", items);
+}
+function getTask() {
+  return localStorage.getItem("newTask").split(",");
+}
+
+// defining function to create list from localStorage after loading the page
+function showTask() {
+  // traversaling on localStorage Array that got by getTsk()function
+  for (const taskText of getTask()) {
+    let newLi = createNew(taskText);
+    newLi.innerHTML += `<input class="checkbox" type="radio" />`;
+    newLi.innerHTML += `<span class="trash"><i class="fa-solid fa-trash-can"></i></span>`;
+    listToDo.appendChild(newLi);
+  }
+}
+showTask();
+
 //======Activate SEARCH input ================================
 searchIcon.addEventListener("click", function () {
   searchInput.classList.toggle("hidden");
@@ -33,17 +66,24 @@ searchIcon.addEventListener("click", function () {
 
 //===== clean the LI value or list Item by trash icon with using EVENT Deligation
 listToDo.addEventListener("click", function (event) {
-  const target = event.target;
+  let target = event.target;
 
   // remove li by click on trash icon
   if (event.target.nodeName === "I") {
+    const li = event.target.closest("li");
+    const text = li.firstChild.textContent;
+    li.remove();
+    items.splice(items.indexOf(text), 1);
+    localStorage.setItem("newTask", items);
     //event.target.parentElement.parentElement.style = "display: none";
-    event.target.closest("li").remove();
+    //event.target.closest("li").remove();
+    //items.splice(items.indexOf(event.target.textContent), 1);
+    //localStorage.setItem("newTask", items);
   }
   // drawing line on the li.value by click on radioButton and checked
   if (
-    target.nodeName === "INPUT" &&
-    (target.type === "checkbox") | (target.type === "radio")
+    (target.nodeName === "INPUT" && target.type === "checkbox") ||
+    target.type === "radio"
   ) {
     target.parentElement.style.textDecoration = target.checked
       ? "line-through"
@@ -55,7 +95,6 @@ listToDo.addEventListener("click", function (event) {
 // mobile support code
 addBtn.addEventListener("touchstart", function () {
   let el = input.value;
-
   let newLi = createNew(el);
   if (input.value == "") {
     alert("please fill the field.");
